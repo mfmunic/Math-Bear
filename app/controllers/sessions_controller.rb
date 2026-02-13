4e2006790265392926d47c90fe90efb8715989d6
Marc Munic <mfmunic@gmail.com>
2026-02-13 13:37:17 -0800

    added index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsController < ApplicationController
+  def show
+  end
+
+  def index
+    @sessions = Session.all
+  end
+
   def new
     @session = Session.new
+  end
+
+  def create
+    @session = Session.new(params["sessions"])
+    @session.save
+    redirect_to sessions_path
+  end
+
+  def destroy
+    @session = Session.find(params[:id])
+    @session.destroy
   end
 end
